a7779fa8d5017a3eefb5b3c332e0229b3f445a92
Dino Letic <dino.letic@jetbrains.com>
2023-05-05 14:29:00 +0200

    ff

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helggddjdalol world!");
+        System.out.println("Helggddjdahhlol world!");
     }
 }
