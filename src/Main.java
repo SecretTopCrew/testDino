9e804d032eb446d7bd0d44be040ceadf8a6fd68d
Dino Letic <dino.letic@jetbrains.com>
2023-05-17 11:26:09 +0200

    Update message in Main.java
    Change the second print statement in the Main class to output a new message. This modification was necessary to improve the readability and relevance of the output, ensuring users receive useful information instead of garbled text.
    
    Update message in Main.java
    
    Change the second print statement in the Main class to output a new message. This modification was necessary to improve the readability and relevance of the output, ensuring users rceive useful information instead of garbled text.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helgffgddjdagghhlol world!");
-        System.out.println("Helgffgddjggdagghfdddhggfdlol world!");
-        System.out.println("Helgffgddjggdjaggfdfdhddhlgol world!");
+        System.out.println("New mesgfggsage!");
+        System.out.println("Helgffgddjggdjaggfdfdhhhddhlgol world!");
+
+
     }
 }
