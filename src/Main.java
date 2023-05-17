d502ee10f47ad36b7aaee229fc03aadc15b4c286
Dino Letic <dino.letic@jetbrains.com>
2023-05-17 11:44:58 +0200

    Removed file completely

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Large file");
-        System.out.println("Small file");
+        System.out.println("No file");
+
 
 
 
