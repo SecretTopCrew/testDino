fbd2e42eb827003da81d52e1de26c5c88edcba55
borrean <48660073+borrean@users.noreply.github.com>
2023-05-19 09:41:01 +0200

    example 2

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Helgffgddjggdjaggfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdjaggfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdjaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjaggfdfdhddhlgol world44!");
     }
 }
