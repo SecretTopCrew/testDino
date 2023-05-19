c16bb90d3a59df47b77e85e153db38fd32c50f08
borrean <elevejt@gmail.com>
2023-05-19 13:59:56 +0200

    czxczx

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Helgffgddjdagghhlol world!");
+        System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddjggdjaggfdfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddjggdjagczczxcgfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjaggfdfdhddhlgol worlffffd44!");
         System.out.println("Helgffgddjdsadczxcxzdsasadsaaadsadgghhlol worldsadddsad!");
         System.out.println("Helgffgdxjfdsfdsdsadgddsfdsgdagghddhlol world!");
         System.out.println("Helgcxzcffgdjggdaggdassahddhlol world!");
