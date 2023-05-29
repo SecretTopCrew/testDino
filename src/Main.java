8b1ae6f2a2bf20220799f79bf9104ffbff5b7bec
borrean <elevejt@gmail.com>
2023-05-29 11:33:43 +0200

    dinotask2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Helgffgddjdagghhlol world!");
         System.out.println("Helgffgddjggdagghfdddhggfdlol world!");
-        System.out.println("Helgffgddjggdjaggfdfdhddhlgol world!");
+        System.out.println("Helgffgddjggdjaggfhhdfdhddhlgol world!");
+        System.out.println("Helgffgddjggdjaggfhhdfdhddhlgol world!");
     }
 }
