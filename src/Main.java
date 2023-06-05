bcd40ad9051918fee88f2e8f228ae3b275267c2b
dinol <elevejt@gmail.com>
2023-06-05 11:19:12 +0200

    dinocomment
    dinocomment
    
    dinocomment

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Helgffgddjdagghhlol world!");
         System.out.println("Helgffgddjggdagghfdddhggfdlol world!");
-        System.out.println("Helgffgddjggdfddjaggfhhdfdhddhlgol world!");
-        System.out.println("Helgfffdsdsfgddjggdjaggfhhdfdhddhlgol world!");
+        System.out.println("Helgffgddjggdfffddsfdsdsfdsddjaggfhhdfdhddhlgol world!");
+        System.out.println("Helgfffdsdhhsfhgddjggdjaggfhhdfdhddhlgol world!");
     }
 }
