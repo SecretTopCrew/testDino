742c5dc8df90178140eb17bef40f49a2428dfaf7
dinol <elevejt@gmail.com>
2023-06-05 11:24:46 +0200

    czxczx

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
         System.out.println("Helgffgddjggdjadsggfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdjfdfaggfdfdhddhlgol world44!");
         System.out.println("Helgffgddjddfggdjaggfdfdhddhlgol world44!");
-        System.out.println("Helgffgddjggdjaggfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddjggdjagddsfdsfgfdfdhddhlgol worlffffd44!");
     }
 }
