9340bb79272358a314d0cb4cb0ca4ded1498e815
dinol <elevejt@gmail.com>
2023-06-09 11:28:55 +0200

    dinocommentg

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     //TODO
     public static void main(String[] args) {
         System.out.println("Helgffgddjdagghhlol world!");
+
+        System.out.println("Helgffgddjdagghhlol world!");
         System.out.println("Helgffgddjfdsdshfhfggdagghfdddhggfdlol world!");
         System.out.println("Helgffgddjdfdsfggdfffddsfdsdsfdsddjaggfhhdfdhddhlgol world!");
         System.out.println("Helgfffdsdhhsfhgddjggdjaggfhhdfdhddhlgol world!");
