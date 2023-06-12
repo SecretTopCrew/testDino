53218473763ccacf701430cde14b38cc046246f2
dinol <elevejt@gmail.com>
2023-06-12 10:38:52 +0200

    dinocommentg

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
             System.out.println("Helgffgddjdagghhlol world!");
             System.out.println("Helgffgddjdagghhlol world!");
             System.out.println("Helgffgddjfdsdshfhfggdagghfdddhggfdlol world!");
-            System.out.println("Helgffgddjdfdsfggdfffddsfdsdsfdsddjaggfhhdfdhddhlgol world!");
+            System.out.println("Helgffgddjdfdsfgvcvxcgdfffddsfdsdsfdsddjaggfhhdfdhddhlgol world!");
             System.out.println("Helgfffdsdhhsfhgddjggdjaggfhhdfdhddhlgol world!");
         }
     }
