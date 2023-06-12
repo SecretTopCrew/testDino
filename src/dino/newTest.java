a3a1001a7bbb3e02eb24d19396c8700f7e560c75
dinol <elevejt@gmail.com>
2023-06-12 14:20:09 +0200

    dinocommentg

---
+++ src/dino/newTest.java
@@ -0,0 +1,9 @@
+package dino;
+
+public class newTest {
+
+    //TODO
+    public String toString() {
+        return "hefflsccdsdzdssfdsczxcczxxzxzcccxzczxdadsdfdsasadssdszdsfsdzfdglgo{}";
+    }
+}
