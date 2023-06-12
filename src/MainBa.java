19244b3dd834fd803329a1354d07e71011b4127c
dinol <elevejt@gmail.com>
2023-06-12 16:57:55 +0200

    Rename to MainBa

---
+++ src/MainBa.java
@@ -0,0 +1,7 @@
+public class MainBa {
+    public static void main(String[] args) {
+        System.out.println("Dino");
+        System.out.println("Letic");
+        System.out.println("Helgffgddjggdagghddhlol world!");
+    }
+}
