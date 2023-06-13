536e90ddfd93db5c9b99af78711fbf25ef2b8c2a
dinol <elevejt@gmail.com>
2023-06-13 15:49:30 +0200

    h

--- src/dino/awdsa.java
+++ src/dino/awdsa.java
@@ -1,10 +1,4 @@
 package dino;
 
 public class awdsa {
-
-    public static void main(String[] args) {
-        System.out.println("Helgffgddjdaggcxzcxzhhlol world!");
-        System.out.println("Helgffgdvxdjfdsfdsgddsfdsgdagghddhlol world!");
-        System.out.println("Helgffgddjggdxzczxcagghddhlol world!");
-    }
 }
