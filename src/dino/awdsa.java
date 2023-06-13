9cbe0f1e245a6c287c6214d6833484736757e496
dinol <elevejt@gmail.com>
2023-06-13 14:23:16 +0200

    Rename to MainBa

--- src/dino/awdsa.java
+++ src/dino/awdsa.java
@@ -3,7 +3,7 @@ package dino;
 public class awdsa {
 
     public static void main(String[] args) {
-        System.out.println("Helgffgddjdagghhlol world!");
+        System.out.println("Helgffgddjdaggcxzcxzhhlol world!");
         System.out.println("Helgffgdvxdjfdsfdsgddsfdsgdagghddhlol world!");
         System.out.println("Helgffgddjggdxzczxcagghddhlol world!");
     }
