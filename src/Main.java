220c41803bdcb78fec04624a64f5216817170e8f
dinol <elevejt@gmail.com>
2023-06-13 14:35:59 +0200

    Rename to MainBa

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helgffgddjddsadczxcxzaagghhlol world!");
+        System.out.println("Helgffgddjddsadczxcxzaadsadgghhlol worldsadsad!");
         System.out.println("Helgffgdvxdjfdsfdsgddsfdsgdagghddhlol world!");
         System.out.println("Helgcxzcffgddjggdagghddhlol world!");
     }
--- src/MainBa.java
+++ src/MainBa.java
@@ -1,6 +1,6 @@
 public class MainBa {
     public static void main(String[] args) {
-        System.out.println("Diczzxcddno");
+        System.out.println("Diczzxcddno sadsadsaddasdsa");
         System.out.println("Letic");
         System.out.println("Helgffgddjggdagghddhlol world!");
     }
