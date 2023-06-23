9818f30a28dcf9d55102cdcdd79ea8287f841476
dinol <elevejt@gmail.com>
2023-06-23 13:55:31 +0200

    g

--- src/MainMaca.java
+++ src/MainMaca.java
@@ -5,10 +5,9 @@ public class MainMaca {
     public static void main(String[] args) {
         {
             System.out.println("Hadsadelgffgddasdsazcxzxdjdagghhlol wosdsadsasarld!");
-            System.out.println("Helgffgddjgdgdagghhlol world!");
             System.out.println("Helgffgddjfdsdshfhfggdagghfdddhggfdlol world!");
             System.out.println("Helgffgddjdfdsfgvcvxcgdfffddsfdsdsfdsddjaggfhhdfdhddhlgol world!");
-            System.out.println("Helgfffdsdhhsfhgddjggdjaggfhhdfdhddhlgol world!");
+            System.out.println("Helgfffdsdhhsfhgddjggdjaggfhhdfdhddhlgolfsadsa world!");
         }
     }
 }
