45c64a191f0b13f778ba2b7663e8e5022f5a0fb8
dinol <elevejt@gmail.com>
2023-06-23 11:58:42 +0200

    h

--- src/dino/dinoMAN.java
+++ src/dino/dinoMAN.java
@@ -4,7 +4,7 @@ public class dinoMAN {
 
     public static void main(String[] args) {
         System.out.println("Helgffgcxzcxzddjdagczxghhlol world!");
-        System.out.println("Helgffgdvxdjfdsfdsgddsfdsgdagghddhlol world!");
+        System.out.println("Helgffgdvxdjfdsfgdgfdsgddsfdsgdagghddhlol world!");
         System.out.println("Helgffgddjggdxzczxcagghddhlol world!");
     }
 
