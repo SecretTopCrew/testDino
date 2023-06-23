2c59cbf56ef8e0ff5033be31d9d255cab263181b
dinol <elevejt@gmail.com>
2023-06-23 13:56:05 +0200

    g

--- src/MainMacaBuca.java
+++ src/MainMacaBuca.java
@@ -1,13 +1,14 @@
-public class MainMacaBuca {
+public class MainMaca {
 
 
     //TODO
     public static void main(String[] args) {
         {
             System.out.println("Hadsadelgffgddasdsazcxzxdjdagghhlol wosdsadsasarld!");
+            System.out.println("Helgffgddjgdgdagghhlol world!");
             System.out.println("Helgffgddjfdsdshfhfggdagghfdddhggfdlol world!");
             System.out.println("Helgffgddjdfdsfgvcvxcgdfffddsfdsdsfdsddjaggfhhdfdhddhlgol world!");
-            System.out.println("Helgfffdsdhhsfhgddjggdjaggfhhdfdhddhlgolfsadsa world!");
+            System.out.println("Helgfffdsdhhsfhgddjggdjaggfhhdfdhddhlgol world!");
         }
     }
 }
