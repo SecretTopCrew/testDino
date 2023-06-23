3857874ec9d06c9bb1d4a88d9a1007de70698c26
dinol <elevejt@gmail.com>
2023-06-23 13:48:03 +0200

    g

--- src/MainMacaKo.java
+++ src/MainMacaKo.java
@@ -1,10 +1,10 @@
-public class MainMaca {
+public class MainMacaKo {
 
 
     //TODO
     public static void main(String[] args) {
         {
-            System.out.println("Hadsadelgffgddasdsazcxzxdjdagghhlol wosdsadsasarld!");
+
             System.out.println("Helgffgddjgdgdagghhlol world!");
             System.out.println("Helgffgddjfdsdshfhfggdagghfdddhggfdlol world!");
             System.out.println("Helgffgddjdfdsfgvcvxcgdfffddsfdsdsfdsddjaggfhhdfdhddhlgol world!");
