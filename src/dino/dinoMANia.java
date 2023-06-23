997fadeb2b0558b6f365c1fdf636edc21c681e33
dinol <elevejt@gmail.com>
2023-06-23 13:39:40 +0200

    g

--- src/dino/dinoMANia.java
+++ src/dino/dinoMANia.java
@@ -1,10 +1,10 @@
 package dino;
 
-public class dinoMAN {
+public class dinoMANia {
 
     public static void main(String[] args) {
         System.out.println("Helgffgcxzcxzddjdagczxghhlol world!");
-        System.out.println("Helgffgdvxdjfdsfgdgfdsgddsfdsgdagghddhlol world!");
+        System.out.println("Helgffgdvxdjffdsfdsdsfgdgfdsgddsfdsgdagghddhlol world!");
         System.out.println("Helgffgddjggdxzczxcaggfddshddhlol world!");
     }
 
