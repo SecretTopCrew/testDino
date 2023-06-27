e2b7d6bfdff95ca1a0c8b7ba8656e64bf5eee3eb
dinol <elevejt@gmail.com>
2023-06-27 14:51:13 +0200

    cxzcxz

--- src/MainBandititis.java
+++ src/MainBandititis.java
@@ -1,6 +1,6 @@
 public class MainBandititis {
     public static void main(String[] args) {
-        System.out.println("Diczzxcddno sadsadsgddaddasdsa");
+        System.out.println("Diczzxcddno sadsadscxzgddaddasdsa");
         System.out.println("Letic");
         System.out.println("Helgffgddjggdagggghddhlol world!");
     }
