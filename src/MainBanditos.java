fd7b9a6b8c68392693e1381d1db3f5fab66db813
dinol <elevejt@gmail.com>
2023-06-27 10:45:39 +0200

    testtask2

--- src/MainBanditos.java
+++ src/MainBanditos.java
@@ -1,7 +1,7 @@
 public class MainBanditos {
     public static void main(String[] args) {
-        System.out.println("Diczzcxvcxcxzxcddno sadsadsgddaddasdsa");
-        System.out.println("Letcxfdsfdssvxcic");
-        System.out.println("dd worfsdffsdvccxld!");
+        System.out.println("Diczzxcddno sadsadsgddaddasdsa");
+        System.out.println("Letic");
+        System.out.println("Helgffgddjggdagggghddhlol world!");
     }
 }
