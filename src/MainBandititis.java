6a2e819062689e3b5bf3f9f8b2564b8f15aadd81
dinol <elevejt@gmail.com>
2023-06-27 10:46:13 +0200

    testtask2

--- src/MainBandititis.java
+++ src/MainBandititis.java
@@ -1,4 +1,4 @@
-public class MainBandit {
+public class MainBandititis {
     public static void main(String[] args) {
         System.out.println("Diczzxcddno sadsadsgddaddasdsa");
         System.out.println("Letic");
