a9909e6e0d12b4369e8d6e100f69e1b39e970cf6
dinol <elevejt@gmail.com>
2023-06-27 10:44:00 +0200

    testtask2

---
+++ src/MainBandit.java
@@ -0,0 +1,7 @@
+public class MainBandit {
+    public static void main(String[] args) {
+        System.out.println("Diczzxcddno sadsadsgddaddasdsa");
+        System.out.println("Letic");
+        System.out.println("Helgffgddjggdagggghddhlol world!");
+    }
+}
