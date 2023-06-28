f61bdadd2dba81198cf0b38ca040b7323f700cd2
dinol <elevejt@gmail.com>
2023-06-28 07:25:25 +0200

    he

--- src/MainBandititis.java
+++ src/MainBandititis.java
@@ -2,6 +2,6 @@ public class MainBandititis {
     public static void main(String[] args) {
         System.out.println("Diczzxcddno sadsadsgddaddasdsa");
         System.out.println("Letic");
-        System.out.println("Helgffgddjggdagggghfsdfdsddhlol world!");
+        System.out.println("Helgffgddjggdagggghddhlol world!");
     }
 }
--- src/MainMaca.java
+++ src/MainMaca.java
@@ -4,10 +4,10 @@ public class MainMaca {
     //TODO
     public static void main(String[] args) {
         {
-            System.out.println("Hadsadelgffgddasdsazcxzxdjdagghhlol wosdsadsasarld!");
+            System.out.println("Hadsadelgffgddasdsazcfsdfxzxdjdagghhlol wosdsadsasarld!");
             System.out.println("Helgffgddjgdgdagghhlol world!");
             System.out.println("Helgffgddjfdsdshfhfggdagghfdddhggfdlol world!");
-            System.out.println("Helgffgddjdfdsfgvcvxfsfsdcgdfffddsfdsdsfdsddjaggfhhdfdhddhlgol world!");
+            System.out.println("Helgffgddjdfdsfgvcvxcgdfffddsfdsdsfdsddjaggfhhdfdhddhlgol world!");
             System.out.println("Helgfffdsdhhsfhgddjggdjaggfhhdfdhddhlgol world!");
         }
     }
