4ed3f12adbeb1440c56fd04dc77ba896f67d1973
dinol <elevejt@gmail.com>
2023-06-29 09:45:17 +0200

    Changes2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Helgffgddjdagghhlol world!");
         System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
-        System.out.println("Helgffgddjggdjaggfdfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddjggdjaggfddfdsfdhddhlgol world4ddd4!");
         System.out.println("Helgffgddjggdjhhhaggfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdjsfdsaggfhhdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdjfdsfsdaggfdfffdhddghgfhlgol world44!");
--- src/MainBandititis.java
+++ src/MainBandititis.java
@@ -2,6 +2,6 @@ public class MainBandititis {
     public static void main(String[] args) {
         System.out.println("Diczzxcddno sadsadsgddaddasdsa");
         System.out.println("Letic");
-        System.out.println("Helgffgddjggdagggghddhlol world!");
+        System.out.println("Helgffgddjfsdfsggdagggghddhlol world!");
     }
 }
