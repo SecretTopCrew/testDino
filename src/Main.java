100a8eb93f266d2e5eef477210e29153be9f71e8
dinol <elevejt@gmail.com>
2023-06-30 10:35:49 +0200

    Changes2

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Helgffgddjggdjaggfddfdsfdhddhlgol world4ddd4!");
         System.out.println("Helgffgddjggdjhhhaggfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdjsfdsaggfhhdfdhddhlgol world44!");
-        System.out.println("Helgffgddjggdjfdsfsdaggfdfffdhddghgfhlgol world44!");
+        System.out.println("Helgffgddjggdjfhghhgdsfsdaggfdfffdhddghgfhlgol world44!");
         System.out.println("Helgffgddjggfdsfdjaggfdfdhddhlgol worlffffd44!");
     }
 }
