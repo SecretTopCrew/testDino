19c6497dd522a39df43b7c21ed8dd76710cda16c
dinol <elevejt@gmail.com>
2023-07-04 14:27:38 +0200

    Changes2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dino");
+        System.out.println("Helgffgddjdagghhlol world!");
         System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
-        System.out.println("Helgffgddjggdjcxxzcaggfdfdhddhlgol world4ddd4!");
-        System.out.println("Helgffxzcxzgddjggdjagczccxzczxcgfczczxdddfdhddhlgol world44!");
-        System.out.println("Helgffgczczxxzcxzddjggdjhhhasdsadggccfdfdhddhlgol world44!");
-        System.out.println("Helgffgddjggdvxvccxvaggcxzczfdfdhddhlgol world44!");
-        System.out.println("xcxzc worlffcxzcxzffd44!");
+        System.out.println("Helgffgddjggdjaggfddfdsfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddjggdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjsfdsaggfhhdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjfhghhgdsfsdaggfdfffdhddghgfhlgol world44!");
+        System.out.println("Helgffgddjggfdsfdjaggfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddjdagghhlol world!");
+        System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddjggdjaggfddfdsfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddjggdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjsfdsaggfhhdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjfhghhgdsfsdaggfdfffdhddghgfhlgol world44!");
+        System.out.println("Helgffgddjggfdsfdjaggfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddjdagghhlol world!");
+        System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddjggdjaggfddfdsfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddjggdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjsfdsaggfhhdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjfhghhgdsfsdaggfdfffdhddghgfhlgol world44!");
+        System.out.println("Helgffgddjggfdsfdjaggfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddjdagghhlol world!");
+        System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddjggdjaggfddfdsfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddjggdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjsfdsaggfhhdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjfhghhgdsfsdaggfdfffdhddghgfhlgol world44!");
+        System.out.println("Helgffgddjggfdsfdjaggfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddjdagghhlol world!");
+        System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddjggdjaggfddfdsfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddjggdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjsfdsaggfhhdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjfhghhgdsfsdaggfdfffdhddghgfhlgol world44!");
+        System.out.println("Helgffgddjggfdsfdjaggfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddjdagghhlol world!");
+        System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddjggdjaggfddfdsfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddjggdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjsfdsaggfhhdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjfhghhgdsfsdaggfdfffdhddghgfhlgol world44!");
+        System.out.println("Helgffgddjggfdsfdjaggfdfdhddhlgol worlffffd44!");
     }
 }
