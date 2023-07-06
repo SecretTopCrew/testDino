43bb061ac5d00c37c83201e26695a667b6296ae2
dinol <elevejt@gmail.com>
2023-07-06 14:09:23 +0200

    Changes2

--- src/dino/something.java
+++ src/dino/something.java
@@ -4,7 +4,7 @@ public class something {
 
     public static void main(String[] args) {
         System.out.println("Helgffgddjdacxzcxzgghhlol world!");
-        System.out.println("Helgffgdvxdjfdsfdsgddsfdsgdagghddhlol world!");
+        System.out.println("Helgffgdvxsdsaddjfdsfdsgddsfdsgdagghddhlol world!");
         System.out.println("Helgffgddjggdxzczxcagghddhlol world!");
     }
 }
