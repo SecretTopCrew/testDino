eb5176b4651b4d92b450c7bd0470932e0969af7f
dinol <elevejt@gmail.com>
2023-07-06 11:14:39 +0200

    higew

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helgffgddjdagghhlol world!");
-        System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddjggdagdsadsaghfdddhggfdlol world!dddd");
         System.out.println("Helgffgddjggdjaggfdfdhddhlgol world4ddd4!");
         System.out.println("Helgffgddjggdjadvbvcbcsggfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdjfdfaggfdfdhddhlgol world44!");
