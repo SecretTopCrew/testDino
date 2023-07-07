f1cae6060c81f982b7a83829f06c26a2518ff1b1
dinol <elevejt@gmail.com>
2023-07-07 12:45:39 +0200

    LalalChange

--- src/MainBugs.java
+++ src/MainBugs.java
@@ -2,6 +2,6 @@ public class MainBugs {
     public static void main(String[] args) {
         System.out.println("dsadsadsaa");
         System.out.println("Letic");
-        System.out.println("Helgffgddjggdagggghddhlol world!");
+        System.out.println("Helgffgddjgjsfdjfjsajasajddsfsd!");
     }
 }
