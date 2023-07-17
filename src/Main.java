3fc8fc41c064a69b6e79e51277190831af71b283
dinol <elevejt@gmail.com>
2023-07-17 12:58:53 +0200

    LalalChangexzxc

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Helgffgddjdagghhlol world!");
         System.out.println("HelgffgddjggxZxZdagghfdddhggfdlol world!dddd");
-        System.out.println("Helgffgddjggdjaggfdfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddjggdjaggfdvcxvfdhddhlgol world4ddd4!");
         System.out.println("Helgffgddjggdjhhhaggfdfdhddhlgol world44!");
-        System.out.println("Helgffgddjggdjaggfhhdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjaggfhhdfczdczdhddhlgol world44!");
         System.out.println("Helgffgddjggdjaggfdfffdhddhlgol world44!");
         System.out.println("Helgffgddjggdjaggfdfdhddhlgol worlffffd44!");
     }
--- src/dino/gfg.kt
+++ src/dino/gfg.kt
@@ -1,4 +1,3 @@
 package dino
 
-class gggfg {
-}
+class gggfg
