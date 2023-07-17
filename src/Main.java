770bbb45e3bb9a6de453b682fb41518875e8a052
dinol <elevejt@gmail.com>
2023-07-17 14:37:34 +0200

    LalalChangexzxc

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Helgffgddjddsadczxcxzaadsadgghhlol worldsadsad!");
-        System.out.println("Helgffgdvxdjfdsfdsgddsfdsgdagghddhlol world!");
-        System.out.println("Helgcxzcffgddjggdagghddhlol world!");
-    }
-}
--- src/MainBanditos.java
+++
@@ -1,7 +0,0 @@
-public class MainBanditos {
-    public static void main(String[] args) {
-        System.out.println("Diczzxcddno sadsadsgddaddasdsa");
-        System.out.println("Letic");
-        System.out.println("Helgffgddjggdagggghddhlol world!");
-    }
-}
