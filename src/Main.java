3afc39bd872451c9a14d345ef2f1c51116f18752
dinol <elevejt@gmail.com>
2023-07-18 13:31:16 +0200

    ggvcxczxcz

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helgffgddjdagghhlol world!");
-        System.out.println("Helgffgddjggdagdsadsaghfdddhggfdlol world!dddd");
-        System.out.println("Helgffgddjggdjaggfdfdhddhlgol world4ddd4!");
-        System.out.println("Helgffgddjggdvcvcxvxxcvcxjadvbvcbcsggfdfdhddhlgol world44!");
-        System.out.println("Helgffgddjggdjsxzcfdxcvcxvfaggfdfdhddhlgol world44!");
-        System.out.println("Helgffgfdfdddxzczxjddfgfdfvcxcxgdfggdjaggfdfdhddhlgol world44!");
-        System.out.println("Helgffgddjggffdcxvcxfdhgdjfdfdfagddsfdsfgfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddjggdjcxxzcaggfdfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddjggdjagczczxcgfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjasdsadggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdvxvccxvaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjaggfdfdhddhlgol worlffffd44!");
     }
 }
