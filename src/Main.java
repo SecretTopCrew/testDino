e78c00909fb3e7f5c9b14290916ffe829d3d7df5
dinol <elevejt@gmail.com>
2023-07-20 10:22:51 +0200

    ggvcxczxczcsadsa
    Signed-off-by: dinol <elevejt@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helgffgddjdagghhlol world!");
-        System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
-        System.out.println("Helgffgddjggdjaggfdfdhddhlgol world4ddd4!");
-        System.out.println("Helgffgddjggdjaggfdfdhddhlgol world44!");
-        System.out.println("Helgffgddjggdjfdfaggfdfdhddhlgol world44!");
-        System.out.println("Helgffgdddfdsjggdjaggfdfdhddhlgol world44!");
-        System.out.println("Helgffgddjggdjczcxzcaggfdfdhddhlgol worlffffd44!");
+        System.out.println("HelgffgddjggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddjggdjaggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddjggdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjaggfdfffdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjaggfdfdhddhlgol worlffffd44!");
     }
 }
