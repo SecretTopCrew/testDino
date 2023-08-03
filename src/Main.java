1342d10f4fe311db19cc99cf33fc1b6287152f76
dinol <elevejt@gmail.com>
2023-08-03 14:09:34 +0200

    Main[Ticket 42] applied changes to

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Helgffgddjdagghhlol world!");
         System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
         System.out.println("Helgffgddjggdjcxxzcaggfdfdhddhlgol world4ddd4!");
-        System.out.println("Helgffgddjggdjagczczxcgfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjcxzggdjagczczxcgfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdjasdsadggfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdvxvccxvaggfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggvxcvcxvcxcxdjaggfdfdhddhlgol worlffffd44!");
