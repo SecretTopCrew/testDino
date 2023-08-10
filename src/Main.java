088717d3f71a9598050baa129e1776332acffb36
borrean <48660073+borrean@users.noreply.github.com>
2023-08-10 14:28:37 +0200

    Update Main.java
    dfdsf

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helgffgddjdagghhlol world!");
-        System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgfdsfdsfddjggdagghfdddhggfdlol world!dddd");
         System.out.println("Helgffgddjggdjcxxzcaggfdfdhddhlgol world4ddd4!");
         System.out.println("Helgffgddjggdjagczczxcgfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdjasdfsdsfdssadasdsadggfdfdhddhlgol world44!");
