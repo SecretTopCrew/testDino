b2cc6484eff978ef0a151854055c8e48331f9206
borrean <48660073+borrean@users.noreply.github.com>
2023-08-10 14:39:20 +0200

    Update Main.java
    ddd

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helgffgddjdagghhlol world!");
+        System.out.println("Helgffgddjdasdsadgghhlol world!");
         System.out.println("Helgffgfdsfdsfddjggdagghfdddhggfdlol world!dddd");
         System.out.println("Helgffgddjggdjcxxdsfdszcaggfdfdhddhlgol world4ddd4!");
         System.out.println("Helgffgddjggdjagczczxcgfdfdhddhlgol world44!");
