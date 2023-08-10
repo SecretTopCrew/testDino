9a8859abccaca0828d1551f9cc37e4e7e3430f66
dinol <elevejt@gmail.com>
2023-08-10 12:48:11 +0200

    Main[Ticket 42] appdaslied changes to

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helgdgdddsadsaasaccvjlol world!");
-        System.out.println("helo dino");
+        System.out.println("Helgffgddjdagghhlol world!");
+        System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddjggdjcxxzcaggfdfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddjggdjagczczxcdadasgfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjasdsadggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdvxvccxvaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjaggfdfdhddhlgol worlffffd44!");
     }
 }
