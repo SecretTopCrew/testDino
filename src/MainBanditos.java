9b0dfd56512d9a9f358098c6f4765fcd6dca5a44
dinol <elevejt@gmail.com>
2023-08-10 12:45:57 +0200

    Main[Ticket 42] applied changes to

--- src/MainBanditos.java
+++ src/MainBanditos.java
@@ -1,7 +1,7 @@
 public class MainBanditos {
     public static void main(String[] args) {
         System.out.println("Diczzxsadasdcddno saadadadsadsgddaddasdsa");
-        System.out.println("Leticd");
-        System.out.println("Helgffgddjgdsadagdfdssdfagggghddhlol world!");
+        System.out.println("Leticddas");
+        System.out.println("Helgffgddjgdsadagdfddaasssdfagggghddhlol world!");
     }
 }
