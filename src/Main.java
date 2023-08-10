9a631c9c636413c74f3c41820cc46c16fec0d052
borrean <48660073+borrean@users.noreply.github.com>
2023-08-10 14:38:00 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Helgffgddjdagghhlol world!");
         System.out.println("Helgffgfdsfdsfddjggdagghfdddhggfdlol world!dddd");
-        System.out.println("Helgffgddjggdjcxxzcaggfdfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddjggdjcxxdsfdszcaggfdfdhddhlgol world4ddd4!");
         System.out.println("Helgffgddjggdjagczczxcgfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdjasdfsdsfdssadasdsadggfdfdhddhlgol world44!");
-        System.out.println("Helgffgddjggdvxvccxvaggfdfdhddhlgol world44!");
-        System.out.println("Helgffgddjggdjaggfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddjggdvxvccxvagfsdfdsgfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjaggfddsfdsfdhddhlgol worlffffd44!");
     }
 }
