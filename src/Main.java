632efe91d413d08a22f3c3941fb751cb8fee36e6
dinol <elevejt@gmail.com>
2023-08-14 11:44:34 +0200

    fixup! Second commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Helgffgddjggddsfsdfagghfdddhggfdlol world!dddd");
         System.out.println("Helgffgddjggdjcxxzcaggfdfdhddfdsdsfdshlgol world4ddd4!");
         System.out.println("Helgffgddjggdjagczczxcdadasgfdfdhddhlgol world44!");
-        System.out.println("Helgffgddjggdjasdsadggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjadsadsasdsadggfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdvxvccxcxzczvaggfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdjaggfdfdhddhlgol worlffffd44!");
     }
