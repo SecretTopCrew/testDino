ab4d1865bbd3cc5b854ee0476367aaf104a66816
dinol <elevejt@gmail.com>
2023-08-16 11:22:35 +0200

    fixup! Second commit

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
         System.out.println("Helgffgddjdasdsadgghhlol world!");
         System.out.println("Helgffgfdsfdsfddjggdagghfdddhggfdlol world!dddd");
         System.out.println("test5");
-        System.out.println("test4");
+        System.out.println("testB");
         System.out.println("test3");
         System.out.println("test2");
-        System.out.println("test1");
+        System.out.println("testX");
     }
 }
