f3ba8bb9f4c88275e3dbe92ae2acf8ef37c44e47
dinol <elevejt@gmail.com>
2023-08-17 17:05:38 +0200

    xvvcrr

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Helgffgddsadsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjaggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddsadsajggvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggfdfdhddhlgol worlffffd44!");
     }
 }
