61a81a5fb43d9f1675de9256bf127f2e308abd04
Dino Lee <dino.letic@jetbrains.com>
2023-08-21 15:09:51 +0200

    LatCommit
    Signed-off-by: Dino Lee <dino.letic@jetbrains.com>

--- src/MainBanditos.java
+++ src/MainBanditos.java
@@ -2,6 +2,6 @@ public class MainBanditos {
     public static void main(String[] args) {
         System.out.println("Diczzcxvcxcxzxcddno sadsadsgddaddasdsa");
         System.out.println("Letcxvxcic");
-        System.out.println("dd worvccxld!");
+        System.out.println("dd worvdcdsasacxld!");
     }
 }
