0032835faf1ee651c9af5036344431ff1580ff79
Dino Lee <dino.letic@jetbrains.com>
2023-08-21 14:31:11 +0200

    commit one
    Signed-off-by: Dino Lee <dino.letic@jetbrains.com>

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Larg file");
-        System.out.println("No sdfsfdfisssdsadxzxzssdhhsasdfdsssCXfsddfgggZCdsaddfdsfdsdCXZCXZssddddsle");
-
-
-
-
-
+        System.out.println("Helgffgddjdagghhlol world!");
+        System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddjggdjcxxzcaggfdfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddjggdjagczczxcgfdddfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjasdsadggccfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdvxvccxvaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgddjggdjaggfdfdhddhlgol worlffffd44!");
     }
 }
