d9ee09185d9cdb0c917976fba1949a1e16c0beb0
dinol <elevejt@gmail.com>
2023-08-22 14:19:06 +0200

    xvvcrrfdfsfdscccc

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Helgffgfdsfdsfddjggdagghfdddhggfdlol world!dddd");
         System.out.println("test5");
         System.out.println("testB");
-        System.out.println("test3");
+        System.out.println("teczxst3");
         System.out.println("test2");
         System.out.println("testX");
     }
--- src/MainBugs.java
+++ src/MainBugs.java
@@ -2,6 +2,6 @@ public class MainBanditos {
     public static void main(String[] args) {
         System.out.println("Diczzxsadasdcddno saadadadsadsgddaddasdsa");
         System.out.println("Leticd");
-        System.out.println("Helgffgddjgdsadagdfdssdfagggghddhlol world!");
+        System.out.println("Helgffgddjgdsadacxcxzcgdfdssdfagggghddhlol world!");
     }
 }
