5c4b2b1dc0e98682e35f5601bc97e15929df8dde
Dino Lee <dino.letic@jetbrains.com>
2023-08-23 11:15:05 +0200

    LatCommit
    Signed-off-by: Dino Lee <dino.letic@jetbrains.com>

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helgffgddjdagghhlol world!");
-        System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
-        System.out.println("Helgffgddjggdjcxxzcaggfdfdhddhlgol world4ddd4!");
+        System.out.println("Helgfcxzfgddjggdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgczxffgddjggdjcxxzcaggfdfdhddhlgol world4ddd4!");
         System.out.println("Helgffgddjggdjagczczxcgfczczxdddfdhddhlgol world44!");
         System.out.println("Helgffgczczxxzcxzddjggdjasdsadggccfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdvxvccxvaggcxzczfdfdhddhlgol world44!");
-        System.out.println("Helgffgddjggdjaggfdcxzcfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddjcxzggdjaggfdcxzcfdhddhlgol worlffffd44!");
     }
 }
