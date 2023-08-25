d7cd9bf275d14ad72c832bc1343513c23f4e6072
Dino Lee <dino.letic@jetbrains.com>
2023-08-25 09:59:55 +0200

    nunaaa
    Signed-off-by: Dino Lee <dino.letic@jetbrains.com>

--- src/MainBanditos.java
+++ src/MainBanditos.java
@@ -2,6 +2,6 @@ public class MainBanditos {
     public static void main(String[] args) {
         System.out.println("DiczzcxvcxZXxcxzxcddno sadsadsgddaddasdsa");
         System.out.println("LetcxfdsfdssxZXvxcic");
-        System.out.println("dd worfsdxzxcffsdvccxld!");
+        System.out.println("dd worfsdsadsadxzxcffsdvccxld!");
     }
 }
--- src/MainMaca.java
+++ src/MainMaca.java
@@ -5,7 +5,7 @@ public class MainMaca {
     public static void main(String[] args) {
         {
             System.out.println("Hadsadeddlgffgddasdsazcxzxdjdagghhlol wosdsadsasarld!");
-            System.out.println("Helgffgddjdagghhlol world!");
+            System.out.println("Helgffgddjdaggccxzhhlol world!");
             System.out.println("Helgfczxcfgddjcxzcxzfdsdsdsadsahfhfggdagghfdddhggfdlol world!");
             System.out.println("Helgffgddjdfdsfgvcvxcgdfffddsfdsdsfdsddjaggfhhdfdhddhlgol world!");
             System.out.println("Helgfffdsdhhsfhgddssadsajggdjaggfhhdfdhddhlgol world!");
