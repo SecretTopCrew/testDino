29fe716e69260664fd6ff0f53d5c40d3a87b1fde
dinol <elevejt@gmail.com>
2023-08-28 15:07:52 +0200

    cc

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Helgffgddjdagghhlol world!");
         System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
         System.out.println("Helgffgddjggdjcxxzcaggfdfdhddhlgol world4ddd4!");
-        System.out.println("Helgffgddjggdjagczccxzczxcgfczczxdddfdhddhlgol world44!");
+        System.out.println("Helgffxzcxzgddjggdjagczccxzczxcgfczczxdddfdhddhlgol world44!");
         System.out.println("Helgffgczczxxzcxzddjggdjasdsadggccfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdvxvccxvaggcxzczfdfdhddhlgol world44!");
         System.out.println("xcxzc worlffffd44!");
