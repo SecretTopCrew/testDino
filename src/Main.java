f6e3c6e3d7fb7a1e6963f9842241e0e78f564611
dinol <elevejt@gmail.com>
2023-08-30 13:49:44 +0200

    commit one
    Signed-off-by: Dino Lee <dino.letic@jetbrains.com>

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ public class Main {
         System.out.println("Helgffgddjdagghhlol world!");
         System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
         System.out.println("Helgffgddjggdjcxxzcaggfdfdhddhlgol world4ddd4!");
-        System.out.println("Helgffgddjggdjagczccxzczxcgfczczxdddfdhddhlgol world44!");
-        System.out.println("Helgffgczczxxzcxzddjggdjasdsadggccfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdczzxjagczccxzczxcgfczczxdddfdhddhlgol world44!");
         System.out.println("Helgffgczczxxzxczxxzcxzddjggdjasdsadggccfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdvxvccxvaggcxzczfdfdhddhlgol world44!");
