5d178b165277269828e3b09ab2f06945a38e605c
dinol <elevejt@gmail.com>
2023-09-12 15:38:40 +0200

    cc

--- src/MainBanditos.java
+++
@@ -1,7 +0,0 @@
-public class MainBanditos {
-    public static void main(String[] args) {
-        System.out.println("Diczzcxvcxcxzxcddno sadsadsgddaddasdsa");
-        System.out.println("Letcxfdsfdssvxcic");
-        System.out.println("dd worfsdffsdvccxld!");
-    }
-}
