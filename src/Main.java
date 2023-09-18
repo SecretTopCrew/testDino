a73ade89b72ce9f921ee7bd6262a9c3d50c2039c
borrean <elevejt@gmail.com>
2023-09-18 21:09:48 +0200

    javo

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Helgffgddjggdagghfdddhggfdlol world!dddd");
         System.out.println("Helgffgddjggdjcxxzcaggfdfdhddhlgol world4ddd4!");
         System.out.println("Helgffxzcxzgddjggdjagczccxzczxcgfczczxdddfdhddhlgol world44!");
-        System.out.println("Helgffgczczxxzcxzddjggdjasdsadggccfdfdhddhlgol world44!");
+        System.out.println("Helgffgczczxxzcxzddjggdjhhhasdsadggccfdfdhddhlgol world44!");
         System.out.println("Helgffgddjggdvxvccxvaggcxzczfdfdhddhlgol world44!");
         System.out.println("xcxzc worlffcxzcxzffd44!");
     }
