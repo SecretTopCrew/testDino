5f60b16acce67a83c019cdd5412b570f0c342702
borrean <elevejt@gmail.com>
2023-10-04 10:07:27 +0200

    commiy 34

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helgffgddsadsajdagghhlol world!");
-        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
-        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
-        System.out.println("Helgffgddsadsajggvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("HelgffgddsadsajggxfdgdfZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjdfgdfacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddsadsajggvgdfcxcdjhhhaggfdfdhddhlgol world44!");
         System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
         System.out.println("Helgffgsadsaddjggdjxcxzaggfdfffdhddhlgol world44!");
-        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Heladsgffgfgdfddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
     }
 }
