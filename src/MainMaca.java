7111217049f590d7ca8a6a3b9836b1768ba1893d
dinoletic <elevejt@gmail.com>
2023-10-30 22:05:16 +0100

    javo

--- src/MainMaca.java
+++ src/MainMaca.java
@@ -4,7 +4,6 @@ public class MainMaca {
     //TODO
     public static void main(String[] args) {
         {
-            System.out.println("test1");
             System.out.println("test2");
             System.out.println("test3");
             System.out.println("test4");
