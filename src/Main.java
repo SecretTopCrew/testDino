e6e05672added7aba31b63be7c750a0dbbf004b0
dinoletic <elevejt@gmail.com>
2023-10-30 22:02:35 +0100

    javo

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Helgffgddjddsadczxcxzaadsadgghhlol worldsadsad!");
         System.out.println("Helgffgdvxdjfdsfdsgddsfdsgdagghddhlol world!");
-        System.out.println("Helgcxzcffgddjggdagghddhlol world!");
+        System.out.println("Helgffgddsadsajdagghhlol world!");
     }
 }
