52f73ba342e7083ddb5012ed4abfa98eca877c38
dinoletic <elevejt@gmail.com>
2023-10-30 22:03:08 +0100

    javo

--- src/MainBanditos.java
+++ src/MainBanditos.java
@@ -1,6 +1,6 @@
 public class MainBanditos {
     public static void main(String[] args) {
-        System.out.println("commit one");
+        System.out.println("commit vccxvone");
         System.out.println("Letic");
         System.out.println("Helgffgddjgjsfdjfjsajasajddsfsd!");
     }
