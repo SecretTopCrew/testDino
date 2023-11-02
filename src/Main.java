9d7ebbfc7e54b19bf66b48bab5d97e2d08e1ba71
dinoletic <elevejt@gmail.com>
2023-11-02 14:20:19 +0100

    https://trello.com/b/QEtHO3Ne/day-4

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HelgffgddsadsajdDSFDSagghhlol world!");
+        System.out.println("HelgffgddsadsajdDSFDSafgghhlol world!");
         System.out.println("HelgffgddsadsajSDFSDggxZxZdagghfdddhggfdlol world!dddd");
         System.out.println("HelgffgddSDFSDadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
         System.out.println("Helgffgddsadsajggvcxcdjhhhaggfdfdhddhlgol world44!");
         System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
         System.out.println("Helgffgsadsaddjggdjxcxzaggfdfffdhddhlgol world44!");
-        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhfddhlgol worlffffd44!");
     }
 }
