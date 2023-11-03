a3cd349772989e2676a8fa5db1885f43d5f94765
dinoletic <elevejt@gmail.com>
2023-11-03 13:47:03 +0100

    Testing

--- src/MainMaca.java
+++ src/MainMaca.java
@@ -4,7 +4,7 @@ public class MainMaca {
     //TODO
     public static void main(String[] args) {
         {
-            System.out.println("Move this");
+            System.out.println("Move thifs");
             System.out.println("Do not fsdsdmove");
             System.out.println("Move this tfsdoo");
         }
