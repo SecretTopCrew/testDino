be3ccdf6e7bd7a4cf0a5a32f8c100f4fec2b0f05
dinoletic <elevejt@gmail.com>
2023-11-03 13:51:42 +0100

    editfsdfsd

--- src/MainMaca.java
+++ src/MainMaca.java
@@ -7,6 +7,8 @@ public class MainMaca {
             System.out.println("Move this 2");
             System.out.println("Do not move");
             System.out.println("Move this too 2");
+            System.out.println("Move this 3");
+            System.out.println("Move this too 3");
         }
     }
 }
