4a3ed96101fe594af2111eec8c3976fcfc6b3e63
dinoletic <elevejt@gmail.com>
2023-11-03 13:47:52 +0100

    editfsdfsd

--- src/MainMaca.java
+++ src/MainMaca.java
@@ -7,6 +7,8 @@ public class MainMaca {
             System.out.println("Move thifs");
             System.out.println("Do not fsdsdmove");
             System.out.println("Move this tfsffdoo");
+            System.out.println("Move this");
+            System.out.println("Move this too");
         }
     }
 }
