4945be20ba5149c517357b0fe84e73d6f5251952
dinoletic <elevejt@gmail.com>
2023-11-03 13:51:47 +0100

    editfsdfsd

--- src/MainMaca.java
+++ src/MainMaca.java
@@ -4,10 +4,8 @@ public class MainMaca {
     //TODO
     public static void main(String[] args) {
         {
-            System.out.println("Move this 2");
-            System.out.println("Do not move");
-            System.out.println("Move this too 2");
             System.out.println("Move this 3");
+            System.out.println("Do not moveee");
             System.out.println("Move this too 3");
         }
     }
