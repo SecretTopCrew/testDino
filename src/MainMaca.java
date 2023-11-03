8a928bc4ddd1319eb30e98029822df4dec3e7fac
dinoletic <elevejt@gmail.com>
2023-11-03 13:52:42 +0100

    Testing

--- src/MainMaca.java
+++ src/MainMaca.java
@@ -4,9 +4,7 @@ public class MainMaca {
     //TODO
     public static void main(String[] args) {
         {
-            System.out.println("Move this 3");
-            System.out.println("Do not moveee");
-            System.out.println("Move this too 3");
+            System.out.println("Move this too 1");
         }
     }
 }
