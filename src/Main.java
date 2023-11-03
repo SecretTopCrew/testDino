4423eb520c0085c539b6b6879d3fa3f4fcd83e73
dinoletic <elevejt@gmail.com>
2023-11-03 10:16:10 +0100

    editfsdfsd

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Helgffgddsadsajdagghhlol world!");
         System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
-        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
-        System.out.println("He  lgffgddsadsajggvcxcdjhhhaggfdfdhddhlgol world44!");
-        System.out.println("Helgffgsads  addjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
-        System.out.println("Helgffgsad  saddjggdjxcxzaggfdfffdhddhlgol world44!");
-        System.out.println("Helads  gffgddjggdjaggxzc  zfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddadszczxajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("He  lgffgddczxsadsajggvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsazxczxds  addjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsczxczad  saddjggdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("czx");
     }
 }
