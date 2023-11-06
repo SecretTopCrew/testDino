2ed1ee2974d69b1b5c9f6f239dfaafde5db129bb
dinoletic <elevejt@gmail.com>
2023-11-06 16:07:08 +0100

    Testing

--- src/MainMaca.java
+++ src/MainMaca.java
@@ -4,8 +4,9 @@ public class MainMaca {
     //TODO
     public static void main(String[] args) {
         {
-            System.out.println("test1116");
-            System.out.println("test22777");
+            System.out.println("Move thfddsis 1");
+            System.out.println("Do not mfsdoveee 1");
+            System.out.println("Move thifssdfdsfdfs too 1");
         }
     }
 }
--- src/dino/dinoMANia.java
+++ src/dino/dinoMANia.java
@@ -3,9 +3,9 @@ package dino;
 public class dinoMANia {
 
     public static void main(String[] args) {
-        System.out.println("Helgffgcxzcxzddjdagczxghhlol world!");
-        System.out.println("Helgffgdvxdjffdsfdsdsfgdgfdsgddsfdsgdagghddhlol world!");
-        System.out.println("Helgffgddjggdxzczxcaggfddshddhlol world!");
+        System.out.println("Helgffgcxzcxzdxvxcdjdagczxghhlol world!");
+        System.out.println("Helgffgdvxdjfxvxcfdsfdsdsf gdgfdsgddsfdsgdagghddhlol world!");
+        System.out.println("Helgffgdvxcdjggdxzczxcaggfddshddhlol world!");
     }
 
 
