3a33d59a6259db2bfb2a6fea8f52df4f2f423b12
dinoletic <elevejt@gmail.com>
2023-11-13 16:34:31 +0100

    dinotest

--- src/MainMaca.java
+++ src/MainMaca.java
@@ -4,8 +4,10 @@ public class MainMaca {
     //TODO
     public static void main(String[] args) {
         {
-            System.out.println("test1116");
-            System.out.println("test22777");
+            System.out.println("test11xzcz16");
+            System.out.println("test22cx777");
+
+
         }
         System.out.println("test22cx777");
     }
