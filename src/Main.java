1b104a644a46a9d0ea0553550497ab53fc71b2e1
dinoletic <elevejt@gmail.com>
2023-12-11 14:18:20 +0100

    dinotest

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,195 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helgffgddsadsajdagghhlol world!");
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
         System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
         System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
-        System.out.println("Helgffgddsadsajggvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
         System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
-        System.out.println("Helgffgsadsaddjggdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
         System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
+
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
+
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
+
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
+
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
+
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
+
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
+
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
+
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
+
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
+
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
     }
 }
