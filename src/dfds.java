9d8414b7ab34df173a77febdb7f3c77d334ca3c8
dinoletic <elevejt@gmail.com>
2023-12-15 09:03:47 +0100

    dinotest

--- src/dfds.java
+++ src/dfds.java
@@ -1,2 +1,9 @@
 public class dfds {
+
+    static void authenticate(GWAccount account) {
+        handleErrors(
+                () -> "authenticate; account: " + account,
+                () -> _authenticate(account)
+        );
+    }
 }
