e2519c5d513e0b32ff7e341b680b67c32b95fd93
dinoletic <elevejt@gmail.com>
2024-01-26 10:17:52 +0100

    dinotestcxzz

--- src/Main.java
+++ src/Main.java
@@ -124,9 +124,9 @@ public class Main {
 
 
 
-        System.out.println("Helgffgddsadsajdagghhlol world!");
-        System.out.println("HelgffgddsadsajggxZsdfsdxZdagghfdddhggfdlol world!dddd");
-        System.out.println("Helgffgddadszczxajgsdfdsgdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("Helgffgddsadsajdagghhlol worldff!");
+        System.out.println("HelgffgddsadsajggxZsdfsdxZdagghfdddhggfdlol world!ffdddd");
+        System.out.println("Helgffgddadszczxajgsdfdsgffdjacxzggfdvcxvfdhddhlgol world4ddd4!");
         System.out.println("He  lgffgddczxsadsajggsdfdsvcxcdjhhhaggfdfdhddhlgol world44!");
         System.out.println("Helgffgsazxczxds  addjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
         System.out.println("Helgffgsczxczad  saddjggdjxcxzaggfdfffdhddhlgol world44!");
@@ -138,7 +138,7 @@ public class Main {
         System.out.println("Helgffgddsadsajdagghhlol world!");
         System.out.println("HelgffgddsadsajggxZsdfsdxZdagghfdddhggfdlol world!dddd");
         System.out.println("Helgffgddadszczxajgsdfdsgdjacxzggfdvcxvfdhddhlgol world4ddd4!");
-        System.out.println("He  lgffgddczxsadsajggsdfdsvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("He  lgffgddczxsadsajggsdfdsvcffxcdjhhhaggfdfdhddhlgol world44!");
         System.out.println("Helgffgsazxczxds  addjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
         System.out.println("Helgffgsczxczad  saddjggdjxcxzaggfdfffdhddhlgol world44!");
         System.out.println("czx");
