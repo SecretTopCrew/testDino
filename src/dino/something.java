e8171e780b6f4ef2caf4f1ac57af2b4e8f32d4ff
dinoletic <elevejt@gmail.com>
2024-02-07 11:05:34 +0100

    n
    Signed-off-by: dinoletic <elevejt@gmail.com>

--- src/dino/something.java
+++ src/dino/something.java
@@ -5,6 +5,6 @@ public class something {
     public static void main(String[] args) {
         System.out.println("Helgffgddjdacxzcxzgghhlol world!");
         System.out.println("Helgffgdvxdjfdsfdsgddsfdsgdagghddhlol world!");
-        System.out.println("Helgffgddjggdxzczxcagghddhlol world!");
+        System.out.println("Helgffgddjgdsdfdsgdxzczxcagghddhlol world!");
     }
 }
