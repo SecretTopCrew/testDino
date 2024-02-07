ca5b29c12e4f54474ead96415265c2818248b4d3
dinoletic <elevejt@gmail.com>
2024-02-07 16:46:07 +0100

    fdfd
    Signed-off-by: dinoletic <elevejt@gmail.com>

--- src/dino/dinoMANia.java
+++ src/dino/dinoMANia.java
@@ -3,9 +3,7 @@ package dino;
 public class dinoMANia {
 
     public static void main(String[] args) {
-        System.out.println("Helgffgcxzcxzddjdagczxghhlol world!");
-        System.out.println("Helgffgdvxdjffdsfdsdsfgdgfdsgddsfdsgdagghddhlol world!");
-        System.out.println("Helgffdsfdsdbbfsdfcbbsdsgddjggdxzczxcaggfddsbvvchddhlol world!");
+
     }
 
 
