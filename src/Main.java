1a4dbfd0e800bbed8d064f1f80ac4acf76d21086
dinoletic <elevejt@gmail.com>
2024-03-05 13:45:30 +0100

    aba
    Signed-off-by: dinoletic <elevejt@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -191,5 +191,21 @@ public class Main {
         System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
         System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
 
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
+        System.out.println("Helgffgddsawerwedsajdagghhlol world!");
+        System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
+        System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
+        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
+        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+
     }
 }
