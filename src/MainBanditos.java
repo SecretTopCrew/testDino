67a110f80972674d042e1a00a68f2d2eefce20f3
dinoletic <elevejt@gmail.com>
2024-03-12 13:30:53 +0100

    fff
    Signed-off-by: dinoletic <elevejt@gmail.com>

--- src/MainBanditos.java
+++ src/MainBanditos.java
@@ -1,6 +1,6 @@
 public class MainBanditos {
     public static void main(String[] args) {
-        System.out.println("commit vccxvone");
+        System.out.println("gurka");
         System.out.println("Letsdfdfssic");
         System.out.println("Helgffgddjgjsdfdssfdsdfdsjfjsajasajddsfsd!");
     }
