e8e5716bdcc136d10b69a85af28f822e4851990d
dinoletic <elevejt@gmail.com>
2024-04-01 15:28:31 +0200

    fg
    Signed-off-by: dinoletic <elevejt@gmail.com>

--- src/BainMaca.java
+++ src/BainMaca.java
@@ -5,6 +5,8 @@ public class BainMaca {
     public static void main(String[] args) {
         {
             System.out.println("Move thfddsis 1");
+
+            //TODO
             System.out.println("Do not mfsdodfdfdveee 1");
             System.out.println("Move thifssdfdsdfdfdfdCSDfdsfsdfs too 1");
             System.out.println("Move thfddsis 1");
