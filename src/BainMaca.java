180452b078ae3931403a793a213d10dc47b58284
dinoletic <elevejt@gmail.com>
2024-04-19 12:45:26 +0200

    CommitTestasdas

--- src/BainMaca.java
+++ src/BainMaca.java
@@ -7,9 +7,9 @@ public class BainMaca {
             System.out.println("Move thfddsis 1");
 
             //TODO
-            System.out.println("Do not mfsdodfdfdveee 1");
-            System.out.println("Move thifssdfdsdfdfdfdCSDfdsfsdfs too 1");
-            System.out.println("Move thfddsis 1");
+            System.out.println("Do not cxzczxmfsdodfdfdveee 1");
+            System.out.println("Move tczxcxzhifssdfdsdfdfdfdCSDfdsfsdfs too 1");
+            System.out.println("Move thzxczxfddsis 1");
             System.out.println("Do not mfsdodfdfdveee 1");
             System.out.println("Move thifssdfdsdfdfdfdCSDfdsfsdfs too 1");
             System.out.println("Move thfddsis 1");
