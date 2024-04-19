d164f5febbbbd03c270d112de11e659d08d2a9b3
dinoletic <elevejt@gmail.com>
2024-04-19 12:48:20 +0200

    superino

--- src/BainMaca.java
+++ src/BainMaca.java
@@ -11,7 +11,7 @@ public class BainMaca {
             System.out.println("Move tczxcxzhifssdfdsdfdfdfdCSDfdsfsdfs too 1");
             System.out.println("Move thzxczxfddsis 1");
             System.out.println("Do not mfsdodfdfdveee 1");
-            System.out.println("Move thifssdfdsdfdfdfdCSDfdsfsdfs too 1");
+            System.out.println("Move thifssdfdsdfdfdsfdsfdfdCSDfdsfsdfs too 1");
             System.out.println("Move thfddsis 1");
             System.out.println("Do not mfsdodfdfdveee 1");
             System.out.println("Move thifssdfdsdfdfdfdCSDfdsfsdfs too 1");
