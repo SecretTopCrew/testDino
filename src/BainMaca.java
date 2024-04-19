c73b7cc62f5cdb1947935abcbaea71c2c3fe8cad
borrean <48660073+borrean@users.noreply.github.com>
2024-04-19 13:19:57 +0200

    Update BainMaca.java

--- src/BainMaca.java
+++ src/BainMaca.java
@@ -10,7 +10,7 @@ public class BainMaca {
             System.out.println("Do not cxzczxmfsdodfdfdveee 1");
             System.out.println("Move tczxcxzhifssdfdsdfdfdfdCSDfdsfsdfs too 1");
             System.out.println("Move thzxczxfddsis 1");
-            System.out.println("Do not mfsdodfdfdveee 1");
+            System.out.println("Do not mfsdodfdfdssdfdsfdveee 1");
             System.out.println("Move thifssdfdsdfdfdsfdsfdfdCSDfdsfsdfs too 1");
             System.out.println("Move thfddsis 1");
             System.out.println("Do not mfsdodfdfdveee 1");
