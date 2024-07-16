cb1b8060f187a974339fbd903a57087a54b94e2a
dinoletic <elevejt@gmail.com>
2024-07-16 15:30:56 +0200

    beeedfsddcxzxcvcxc

--- src/BainMaca.java
+++ src/BainMaca.java
@@ -47,7 +47,7 @@ public class BainMaca {
             System.out.println("Move thifssdfdsdfdfdfdCSDfdsfsdfs too 1");
             System.out.println("Move thfddsis 1");
             System.out.println("Do not mfsdodfdfdveee 1");
-            System.out.println("Move thifssdfdsdfdfdfdCSDfdsfsdfs too 1");
+            System.out.println("Move tdsashifssdfdsdfdfdfdCSDfdsfsdfs too 1");
             System.out.println("Move thfddsis 1");
             System.out.println("Do not mfsdodfdfdveee 1");
             System.out.println("Move thifssdfdsdfdfdfdCSDfdsfsdfs too 1");
