c59a59e19162f08cc8e7a24804717cc638606b07
dinoletic <elevejt@gmail.com>
2024-07-24 13:21:09 +0200

    dinotask

--- src/MainBanditos.java
+++ src/MainBanditos.java
@@ -1,1 +1,7 @@
-public class MainBanditos {    public static void main(String[] args) {        System.out.println("gurka");        System.out.println("Letsdfdfssic");        System.out.println("Helgffgddjgjsdfdssfdsdfdsjfjsajasajddsfsd!");    }}
+public class MainBanditos {
+    public static void main(String[] args) {
+        System.out.println("gurka");
+        System.out.println("Letsdfdfssic");
+        System.out.println("Helgffgddjgjsdcccfdssfdsdfdsjfjsajasajddsfsd!");
+    }
+}
