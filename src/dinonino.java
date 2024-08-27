3a68e3cfb42366e0bedef3800b51dc790f03b4fa
dinoletic <elevejt@gmail.com>
2024-08-27 12:13:00 +0200

    dinotaskaaa

--- src/dinonino.java
+++ src/dinonino.java
@@ -3,10 +3,10 @@ public class dinonino {
         System.out.println("Helgffgddsbvvcbawerwedsajdagghhlol world!");
         System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
         System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
-        System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
+        System.out.println("HelgffgddsawerwerdsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
         System.out.println("Helgffgsadsaddjzxczxczxggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
         System.out.println("Helgffgsadsadhjdjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
-        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Heladsgffgddjggdrwerwerewjaggxzcxzfdfdhddhlgol worlffffd44!");
         System.out.println("Helgffgddsawerwedsajcxzzxcdagghhlol world!");
         System.out.println("HelgffgddsadsajggxZxZdxzxzcxzagghfdddhggfdlol world!dddd");
         System.out.println("Helgffgddadsajgczxcgdjacxzggfdvcxvfdhddhlgol world4ddd4!");
