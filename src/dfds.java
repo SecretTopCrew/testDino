55824d8f93720bf12cd4d6ad5c25819610a1c60d
dinoletic <elevejt@gmail.com>
2024-09-02 14:43:54 +0200

    aaaaasadsa

---
+++ src/dfds.java
@@ -0,0 +1,9 @@
+public class dfds {
+
+    static void authenticate(GWAccount account) {
+        handleErrors(dfgdgd
+                () -> "authenticate; account: " + account,
+                () -> _authenticate(account)c
+        );
+    }
+}
