c07f364631963c51c5f244cd16c6aaa7cfeb387c
dinoletic <elevejt@gmail.com>
2025-02-07 19:11:11 +0100

    axssaxasfsfdssdsadsassfsddasa
    Signed-off-by: dinoletic <elevejt@gmail.com>

--- src/BainMaca.java
+++ src/BainMaca.java
@@ -216,7 +216,7 @@ public class BainMaca {
             System.out.println("Move thfddsis 1");
             System.out.println("Do not mfsdodfdfdveee 1");
             System.out.println("Move thifssdfdsdfdfdfdCSDfdsfsdfs too 1");
-            System.out.println("Move thfddsis 1");
+            System.out.println("Move thfddsis 1"sada);
             System.out.println("Do not mfsdodfdfdveee 1");
             System.out.println("Move thifssdfdsdfdfdfdCSDfdsfsdfs too 1");
             System.out.println("Move thfddsis 1");
