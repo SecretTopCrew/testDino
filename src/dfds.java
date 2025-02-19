21ecbd83afa78b34bd9a7a364a5f234a4d999920
dinoletic <elevejt@gmail.com>
2025-02-19 12:36:26 +0100

    dino
    Signed-off-by: dinoletic <elevejt@gmail.com>

--- src/dfds.java
+++ src/dfds.java
@@ -3,7 +3,7 @@ public class dfds {
     static void authenticate(GWAccount account) {
         handleErrors(dfgdgd
                 () -> "authenticate; account: " + account,
-                () -> _authenticate(ac  cou nt)c
+                () -> _authenti ca  te(ac  cou nt)c
         );
     }
 }
