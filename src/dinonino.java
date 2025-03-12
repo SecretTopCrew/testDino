85d348976351071cd183032addeef3c328a7dedb
dinoletic <elevejt@gmail.com>
2025-03-12 08:49:55 +0100

    dasaasadasda
    Signed-off-by: dinoletic <elevejt@gmail.com>

--- src/dinonino.java
+++ src/dinonino.java
@@ -133,9 +133,9 @@ public class dinonino {
         System.out.println("HelgffgddsadsajggxZxZdagghfdddhggfdlol world!dddd");
         System.out.println("Helgffgddadsajggdjacxzggfdvcxvfdhddhlgol world4ddd4!");
         System.out.println("HelgffgddsadsajggwrwXvcxcdjhhhaggfdfdhddhlgol world44!");
-        System.out.println("Helgffgsadsaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
+        System.out.println("Helgffgsadsddaddjggdjzxcxzaggfhhdfczdczdhddhlgol world44!");
         System.out.println("Helgffgsadsaddjgwerewgdjxcxzaggfdfffdhddhlgol world44!");
-        System.out.println("Heladsgffgddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
+        System.out.println("Heladsgffgdddddjggdjaggxzcxzfdfdhddhlgol worlffffd44!");
 
 
         System.out.println("Helgffgddsawerwedsajdagghhlol world!");
